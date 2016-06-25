819947384aa6aae11ef0fced4fd8e98d07fee9d1
Jason Gross <jgross@mit.edu>
2016-06-25 13:12:22 -0400

    Add more prereqs

--- 00-install-prereqs.sh
+++ 00-install-prereqs.sh
@@ -5,4 +5,4 @@ if [ $# -ne 0 ]; then
     ARGS="--show-progress"
 fi
 
-sudo apt-get install "$@" $ARGS git build-essential devscripts debhelper
+sudo apt-get install "$@" $ARGS git build-essential devscripts debhelper dh-ocaml ocaml-nox ocaml-best-compilers ocaml-findlib camlp5 liblablgtk2-ocaml-dev liblablgtksourceview2-ocaml-dev texlive-latex-extra hevea

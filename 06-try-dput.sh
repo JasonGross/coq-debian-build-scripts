95b117c6ce5a12fcf11d0974402feb5b8583fbb3
Jason Gross <jgross@mit.edu>
2016-06-26 01:26:59 -0400

    Flip order of upload
    We care more about the ocaml 3 version of coq 8.4

--- 06-try-dput.sh
+++ 06-try-dput.sh
@@ -9,10 +9,10 @@ for i in $VERSIONS; do
   ARCHIVE="$(to_archive_name "$i")"
   pushd "debian-sources/$FOLDER" || exit $?
   if [ "$TARGET" == trusty ]; then
-    dput ppa:jgross-h/many-coq-versions "${ARCHIVE}${PPA_EXT}_source.changes"
     if [[ "$i" == 8.4* ]]; then
       dput ppa:jgross-h/many-coq-versions-ocaml-3 "${ARCHIVE}${PPA_EXT}_source.changes"
     fi
+    dput ppa:jgross-h/many-coq-versions "${ARCHIVE}${PPA_EXT}_source.changes"
   elif [ "$TARGET" == precise ]; then
     dput ppa:jgross-h/many-coq-versions-ocaml-3 "${ARCHIVE}${PPA_EXT}_source.changes"
   fi

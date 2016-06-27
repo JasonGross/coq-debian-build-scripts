3ab875dfd2ed0697ca94be0eca8310485cc66229
Jason Gross <jgross@mit.edu>
2016-06-27 14:23:02 -0400

    Use a different repo

--- 06-try-dput.sh
+++ 06-try-dput.sh
@@ -8,13 +8,14 @@ for i in $VERSIONS; do
   FOLDER="$(to_folder_name "$i")"
   ARCHIVE="$(to_archive_name "$i")"
   pushd "debian-sources/$FOLDER" || exit $?
+  OCAML3_PPA="many-coq-versions-ocaml-3-temp-while-over-quota-2"
   if [ "$TARGET" == trusty ]; then
     if [[ "$i" == 8.4* ]]; then
-      dput ppa:jgross-h/many-coq-versions-ocaml-3-temp-while-over-quota "${ARCHIVE}${PPA_EXT}_source.changes"
+      dput ppa:jgross-h/$OCAML3_PPA "${ARCHIVE}${PPA_EXT}_source.changes"
     fi
     dput ppa:jgross-h/many-coq-versions "${ARCHIVE}${PPA_EXT}_source.changes"
   elif [ "$TARGET" == precise ]; then
-    dput ppa:jgross-h/many-coq-versions-ocaml-3-temp-while-over-quota "${ARCHIVE}${PPA_EXT}_source.changes"
+    dput ppa:jgross-h/$OCAML3_PPA "${ARCHIVE}${PPA_EXT}_source.changes"
   fi
   #sleep 2m
   popd

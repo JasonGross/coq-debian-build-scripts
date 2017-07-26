112e40ee4fb72fa2642e101504a362fe3faad9db
Jason Gross <jgross@mit.edu>
2017-07-25 20:43:02 -0400

    Bump version

--- run.sh
+++ run.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-
-for TARGET in trusty precise; do
-    export TARGET=$TARGET
-    ./04-make-debian-from-reference.sh && ./05-build-debian-only-source.sh && ./06-try-dput.sh
-done
-./bump-version.sh
+for TARGET in trusty precise; do export TARGET; ./04-make-debian-from-reference.sh && ./05-build-debian-only-source.sh && ./06-try-dput.sh; done && ./bump-version.sh && git commit -am "Bump version" && git push

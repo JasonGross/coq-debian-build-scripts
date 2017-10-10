0132e7211595e976e11e58d0a0ba77697d4167dc
Jason Gross <jgross@mit.edu>
2017-10-10 05:39:22 -0400

    Fix broken git dep

--- versions.sh
+++ versions.sh
@@ -11,7 +11,7 @@ if [ "$TARGET" != trusty -a "$TARGET" != precise ]; then
   TARGET=trusty # precise #
 fi
 
-PPA_EXT="-1~${TARGET}~ppa77"
+PPA_EXT="-1~${TARGET}~ppa79"
 
 function to_debian_version() {
   echo "$1" | sed s'/-\(rc\)/\1/g' | sed s'/+\(beta\)/\1/g' | sed s'/-\(beta\)/\1/g' | sed s'/-\(alpha\)/\1/g' | sed s'/+\(alpha\)/\1/g' | sed s'/\(rc\)/~\1/g' | sed s'/\(beta\)/~\1/g' | sed s'/\(alpha\)/~\1/g'

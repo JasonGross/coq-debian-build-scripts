c1c795b728220ae0aaadf2d211b69cd0f8897b91
Jason Gross <jgross@mit.edu>
2017-10-18 10:33:41 -0400

    Coq 8.7.0

--- 01-download-coqs.sh
+++ 01-download-coqs.sh
@@ -12,6 +12,12 @@ cd coq-source
 #  cp -f V$i.tar.gz coq-${i//+/-}.tar.gz || exit $?
 #done
 
+for i in $GITHUB_VERSIONS; do
+  wget -N https://github.com/coq/coq/archive/V$i.tar.gz || exit $?
+  tar -xzf V$i.tar.gz || exit $?
+  rm -rf coq-$i.tar.gz && tar -czf coq-$i.tar.gz coq-$i || exit $?
+done
+
 for i in $GITHUB_MINUS_TO_PLUS_VERSIONS; do
   wget -N https://github.com/coq/coq/archive/V$i.tar.gz || exit $?
   rm -rf coq-${i//+/-} coq-$i && tar -xzf V$i.tar.gz || exit $?

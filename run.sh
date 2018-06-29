311004152b17906e6f24bc9f8eadfc8c920184d6
Jason Gross <jgross@mit.edu>
2018-06-29 11:00:55 -0400

    Bump version

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
- for TARGET in trusty precise bionic artful zesty xenial; do export TARGET; ./04-make-debian-from-reference.sh && ./05-build-debian-only-source.sh && ./06-try-dput.sh; done && ./bump-version.sh && git commit -am "Bump version" && git push
-#for TARGET in bionic artful zesty xenial; do export TARGET; ./04-make-debian-from-reference.sh && ./05-build-debian-only-source.sh && ./06-try-dput.sh; done && ./bump-version.sh && git commit -am "Bump version" && git push
+#!/usr/bin/env bash
+for TARGET in trusty precise cosmic bionic artful xenial; do export TARGET; ./04-make-debian-from-reference.sh && ./05-build-debian-only-source.sh && ./06-try-dput.sh; done && ./bump-version.sh && git commit -am "Bump version" && git push
+#for TARGET in cosmic; do export TARGET; ./04-make-debian-from-reference.sh && ./05-build-debian-only-source.sh && ./06-try-dput.sh; done && ./bump-version.sh && git commit -am "Bump version" && git push

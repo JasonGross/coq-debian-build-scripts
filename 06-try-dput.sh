c3f05ebdd3f5ffd8b5e9f254bbaa1c0864c7d0d4
Jason Gross <jgross@mit.edu>
2019-12-06 21:29:04 -0500

    Bump version

--- 06-try-dput.sh
+++ 06-try-dput.sh
@@ -19,7 +19,7 @@ for i in $VERSIONS; do
       dput ppa:jgross-h/$OCAML4_05_PPA "${ARCHIVE}${PPA_EXT}_source.changes"
     fi
   elif [ "$TARGET" == precise ]; then
-      if [[ "$i" == 8.* ]] && [[ "$i" != 8.4* ]] && [[ "$i" != 8.3* ]] && [[ "$i" != 8.2* ]] && ( [[ "$i" == 8.10* ]] || [[ "$i" != 8.1* ]]) && [[ "$i" != 8.0* ]]; then
+      if [[ "$i" == 8.* ]] && [[ "$i" != 8.4* ]] && [[ "$i" != 8.3* ]] && [[ "$i" != 8.2* ]] && ( [[ "$i" == 8.12* ]] || [[ "$i" == 8.11* ]] || [[ "$i" == 8.10* ]] || [[ "$i" != 8.1* ]]) && [[ "$i" != 8.0* ]]; then
         if [[ "$i" == 8.5* ]] || [[ "$i" == 8.6* ]] || [[ "$i" == 8.7* ]] || [[ "$i" == 8.8* ]] || [[ "$i" == 8.9* ]]; then
           dput ppa:jgross-h/many-coq-versions "${ARCHIVE}${PPA_EXT}_source.changes"
         else

13cd8cbc0bcc28bdaa7808d0b562e039cd934ee8
Jason Gross <jgross@mit.edu>
2020-06-29 18:24:16 -0400

    Remove obsolete disco

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-for TARGET in trusty precise groovy eoan disco bionic xenial; do export TARGET; ./04-make-debian-from-reference.sh && ./05-build-debian-only-source.sh && ./06-try-dput.sh; done && ./bump-version.sh && git commit -am "Bump version" && git push
+for TARGET in trusty precise groovy eoan bionic xenial; do export TARGET; ./04-make-debian-from-reference.sh && ./05-build-debian-only-source.sh && ./06-try-dput.sh; done && ./bump-version.sh && git commit -am "Bump version" && git push
 #for TARGET in disco; do export TARGET; ./04-make-debian-from-reference.sh && ./05-build-debian-only-source.sh && ./06-try-dput.sh; done && ./bump-version.sh && git commit -am "Bump version" && git push

a083b6219469914d1f7bfe7bc5e05d7eb0dcc539
Jason Gross <jgross@mit.edu>
2021-02-02 01:51:40 -0500

    Bump version

--- 06-try-dput.sh
+++ 06-try-dput.sh
@@ -8,7 +8,7 @@ for i in $VERSIONS; do
   FOLDER="$(to_folder_name "$i")"
   ARCHIVE="$(to_archive_name "$i")"
   pushd "debian-sources/$FOLDER" || exit $?
-  OCAML3_PPA="many-coq-versions-ocaml-3-temp-while-over-quota"
+  OCAML3_PPA="many-coq-versions-ocaml-3"
   OCAML4_05_PPA="many-coq-versions-ocaml-4-05"
   if vercmp "$i" "<" "8.5~"; then
     dput ppa:jgross-h/$OCAML3_PPA "${ARCHIVE}${PPA_EXT}_source.changes"
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 ./02-make-debian-preorigs.sh && ./03-sanitize-debian-origs.sh
-NO_ORIG_SOURCE="-no-orig-source" # ""
-for TARGET in hirsute groovy focal bionic xenial trusty precise; do export TARGET; ./04-make-debian-from-reference.sh && ./05-build-debian-only-source${NO_ORIG_SOURCE}.sh && ./06-try-dput.sh; done && ./bump-version.sh && git commit -am "Bump version" && git push
+NO_ORIG_SOURCE="" # "-no-orig-source" # ""
+#for TARGET in hirsute groovy focal bionic xenial trusty precise; do export TARGET; ./04-make-debian-from-reference.sh && ./05-build-debian-only-source${NO_ORIG_SOURCE}.sh && ./06-try-dput.sh; done && ./bump-version.sh && git commit -am "Bump version" && git push
+for TARGET in focal; do export TARGET; ./04-make-debian-from-reference.sh && ./05-build-debian-only-source${NO_ORIG_SOURCE}.sh && ./06-try-dput.sh; done && ./bump-version.sh && git commit -am "Bump version" && git push
 #for TARGET in hirsute groovy focal; do export TARGET; ./04-make-debian-from-reference.sh && ./05-build-debian-only-source.sh && ./06-try-dput.sh; done && ./bump-version.sh && git commit -am "Bump version" && git push

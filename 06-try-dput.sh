f46568528402cd0096c861917bdc2244623af4a8
Jason Gross <jgross@mit.edu>
2021-02-01 19:05:40 -0500

    Simplify ppa selection logic

--- 06-try-dput.sh
+++ 06-try-dput.sh
@@ -10,30 +10,12 @@ for i in $VERSIONS; do
   pushd "debian-sources/$FOLDER" || exit $?
   OCAML3_PPA="many-coq-versions-ocaml-3-temp-while-over-quota"
   OCAML4_05_PPA="many-coq-versions-ocaml-4-05"
-  if [ "$TARGET" == trusty ]; then
-    if [[ "$i" == 8.4* ]]; then
-      dput ppa:jgross-h/$OCAML3_PPA "${ARCHIVE}${PPA_EXT}_source.changes"
-    elif vercmp "$i" ">=" "8.5~" && vercmp "$i" "<" "8.10~"; then
-      dput ppa:jgross-h/many-coq-versions "${ARCHIVE}${PPA_EXT}_source.changes"
-    else
-      dput ppa:jgross-h/$OCAML4_05_PPA "${ARCHIVE}${PPA_EXT}_source.changes"
-    fi
-  elif [ "$TARGET" == precise ]; then
-      if vercmp "$i" ">=" "8.5~"; then
-        if vercmp "$i" "<" "8.10~"; then
-          dput ppa:jgross-h/many-coq-versions "${ARCHIVE}${PPA_EXT}_source.changes"
-        else
-          dput ppa:jgross-h/$OCAML4_05_PPA "${ARCHIVE}${PPA_EXT}_source.changes"
-        fi
-    else
-      dput ppa:jgross-h/$OCAML3_PPA "${ARCHIVE}${PPA_EXT}_source.changes"
-    fi
+  if vercmp "$i" "<" "8.5~"; then
+    dput ppa:jgross-h/$OCAML3_PPA "${ARCHIVE}${PPA_EXT}_source.changes"
+  elif vercmp "$i" "<" "8.10~"; then
+    dput ppa:jgross-h/many-coq-versions "${ARCHIVE}${PPA_EXT}_source.changes"
   else
-    if vercmp "$i" ">=" "8.5~" && vercmp "$i" "<" "8.10~"; then
-      dput ppa:jgross-h/many-coq-versions "${ARCHIVE}${PPA_EXT}_source.changes"
-    else
-      dput ppa:jgross-h/$OCAML4_05_PPA "${ARCHIVE}${PPA_EXT}_source.changes"
-    fi
+    dput ppa:jgross-h/$OCAML4_05_PPA "${ARCHIVE}${PPA_EXT}_source.changes"
   fi
   #sleep 2m
   popd

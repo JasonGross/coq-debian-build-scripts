ed71e0ba135d12b0ec0724ea792d722f378b9da6
Jason Gross <jgross@mit.edu>
2021-11-11 21:54:52 -0800

    Bump version

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #./02-make-debian-preorigs.sh && ./03-sanitize-debian-origs.sh
 NO_ORIG_SOURCE="" # "-no-orig-source" # ""
-TARGETS="jammy impish hirsute focal bionic xenial trusty" # "hirsute groovy xenial"
+TARGETS="jammy impish" # "jammy impish hirsute focal bionic xenial trusty" # "hirsute groovy xenial"
 NEW_PPAS="many-coq-versions-ocaml-4-08 many-coq-versions-ocaml-4-11"
-for NEW_PPA in ${NEW_PPAS}; do export NEW_PPA; for TARGET in $TARGETS; do export TARGET; ./04-make-debian-from-reference.sh && ./05-build-debian-only-source${NO_ORIG_SOURCE}.sh && ./06-try-dput.sh || exit $?; done && ./bump-version.sh && git commit -am "Bump version" && git push; done
+for NEW_PPA in ${NEW_PPAS}; do export NEW_PPA; export VER_LT_8_14_PPA="${NEW_PPA}"; for TARGET in $TARGETS; do export TARGET; ./04-make-debian-from-reference.sh && ./05-build-debian-only-source${NO_ORIG_SOURCE}.sh && ./06-try-dput.sh || exit $?; done && ./bump-version.sh && git commit -am "Bump version" && git push; done

87961b2140f419d8dc492741f45a0c17a700d9c8
Jason Gross <jgross@mit.edu>
2022-11-26 15:22:34 -0500

    Bump version

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #./02-make-debian-preorigs.sh && ./03-sanitize-debian-origs.sh
 NO_ORIG_SOURCE="" # "-no-orig-source" # ""
-TARGETS="lunar kinetic jammy focal bionic xenial trusty"
-NEW_PPAS="many-coq-versions-ocaml-4-11" # "many-coq-versions-ocaml-4-08 many-coq-versions-ocaml-4-11"
+TARGETS="lunar kinetic jammy" # "lunar kinetic jammy focal bionic xenial trusty"
+NEW_PPAS="many-coq-versions-ocaml-4-08 many-coq-versions-ocaml-4-11" # "many-coq-versions-ocaml-4-11" # "many-coq-versions-ocaml-4-08 many-coq-versions-ocaml-4-11"
 for NEW_PPA in ${NEW_PPAS}; do export NEW_PPA; export VER_LT_8_14_PPA="${NEW_PPA}"; export VER_LT_8_16_PPA="${NEW_PPA}"; for TARGET in $TARGETS; do export TARGET; ./04-make-debian-from-reference.sh && ./05-build-debian-only-source${NO_ORIG_SOURCE}.sh && ./06-try-dput.sh || exit $?; done && ./bump-version.sh && git commit -am "Bump version" && git push; done
